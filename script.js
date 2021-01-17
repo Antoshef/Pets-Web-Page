1867853286ada7be942021a8b680df9710c7fac5
Anton Stanev <antoshef21@gmail.com>
2021-01-17 08:43:37 +0200

    Pets

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+// Button Call Pets Dropdown
 function petsBar() {
     let next = document.getElementsByClassName('header-navigation__dropdown')[1];
     if (!next.classList.value.includes('u-dropdown--hidden')) {
@@ -12,6 +13,7 @@ function petsBar() {
     };
 };
 
+// Button Call Resourses Dropdown
 function resoursesBar() {
     let next = document.getElementsByClassName('header-navigation__dropdown')[0];
     if (!next.classList.value.includes('u-dropdown--hidden')) {
@@ -25,13 +27,32 @@ function resoursesBar() {
     };
 };
 
+// Button Call Search Bar
 function search() {
-    let current = document.getElementsByClassName('search-bar__menu')[0];
+    let current = document.getElementsByClassName('search-bar__menu')[1];
     current.classList.add('u-search--show');
     document.getElementById('selected').focus();
 }
 
+// Button Close Search Bar
 function closeSearch() {
-    let current = document.getElementsByClassName('search-bar__menu')[0];
+    let current = document.getElementsByClassName('search-bar__menu')[1];
     current.classList.remove('u-search--show');
-}
+};
+
+// Button Call Burger Menu Dropdown
+function showBurger() {
+    let dropdownMenu = document.getElementsByClassName('header-navigation__dropdown');
+    for (let i = 0; i < dropdownMenu.length; i++) {
+        let menu = dropdownMenu[i];
+        if (menu.classList.value.includes('u-flex')) {
+            menu.classList.remove('u-flex');
+            let btnChange = document.getElementsByClassName('fa fa-times')[0];
+            btnChange.className = 'fa fa-bars';
+        } else {
+            menu.classList.add('u-flex');
+            let btnChange = document.getElementsByClassName('fa fa-bars')[0];
+            btnChange.className = 'fa fa-times';
+        };
+    };
+};
