3c24372b3d73d1089856504ab63166570ccbc49d
Anton Stanev <antoshef21@gmail.com>
2021-01-17 20:35:31 +0200

    Pets

--- script.js
+++ script.js
@@ -2,42 +2,42 @@
 // Button Call Pets Dropdown
 function petsBar() {
     let next = document.getElementsByClassName('header-navigation__dropdown')[1];
-    if (!next.classList.value.includes('u-dropdown--hidden')) {
-        next.classList.add('u-dropdown--hidden');
+    if (!next.classList.value.includes('u-hidden')) {
+        next.classList.add('u-hidden');
     };
     let current = document.getElementsByClassName('header-navigation__dropdown')[0];
-    if (current.classList.value.includes('u-dropdown--hidden')) {
-        current.classList.remove('u-dropdown--hidden');
+    if (current.classList.value.includes('u-hidden')) {
+        current.classList.remove('u-hidden');
     } else {
-        current.classList.add('u-dropdown--hidden');
+        current.classList.add('u-hidden');
     };
 };
 
 // Button Call Resourses Dropdown
 function resoursesBar() {
     let next = document.getElementsByClassName('header-navigation__dropdown')[0];
-    if (!next.classList.value.includes('u-dropdown--hidden')) {
-        next.classList.add('u-dropdown--hidden');
+    if (!next.classList.value.includes('u-hidden')) {
+        next.classList.add('u-hidden');
     };
     let current = document.getElementsByClassName('header-navigation__dropdown')[1];
-    if (current.classList.value.includes('u-dropdown--hidden')) {
-        current.classList.remove('u-dropdown--hidden');
+    if (current.classList.value.includes('u-hidden')) {
+        current.classList.remove('u-hidden');
     } else {
-        current.classList.add('u-dropdown--hidden');
+        current.classList.add('u-hidden');
     };
-};
+}; 
 
 // Button Call Search Bar
 function search() {
     let current = document.getElementsByClassName('search-bar__menu')[1];
-    current.classList.add('u-search--show');
+    current.classList.add('u-flex');
     document.getElementById('selected').focus();
 }
 
 // Button Close Search Bar
 function closeSearch() {
     let current = document.getElementsByClassName('search-bar__menu')[1];
-    current.classList.remove('u-search--show');
+    current.classList.remove('u-flex');
 };
 
 // Button Call Burger Menu Dropdown
